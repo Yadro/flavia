965159bdcbd1ae2b59b8ee3efa18c1b2c2d347c2
yadro <yadrone@gmail.com>
2016-03-12 22:44:07 +0300

    init from seona

---
+++ src/app.tsx
@@ -0,0 +1,56 @@
+///<reference path="../typings/main.d.ts"/>
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+//import {InputMatrix} from 'components/input_matrix'
+import {Matrix} from 'helper/matrix';
+//import Fraction = require('../node_modules/fraction.js/fraction');
+//import {Graph} from './components/graph';
+
+//import {Fraction} from "helper/fraction.js.ts";
+
+let test = [
+    [0,0,0],
+    [0,1,3],
+    [1,2,2],
+    [7,2,10],
+    [1,1,1],
+];
+
+interface AppState {
+    matrix: Matrix;
+}
+
+class App extends React.Component<any, AppState> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            matrix: new Matrix(3, 3)
+        };
+    }
+
+    callback(matrix) {
+        let m = matrix.gauss(true);
+        m.debugMatrix.forEach(e => {
+            console.info(e.operation.toString());
+            console.log(e.matrix.toString());
+        });
+        console.log(m.toString());
+        this.setState({matrix: matrix});
+    }
+
+    render() {
+        /*
+         <h1>Just apply Gauss it</h1>
+         <InputMatrix matrix={this.state.matrix} callback={this.callback.bind(this)}/>
+        */
+        return (
+            <div>
+                <h1>123456789</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.react'));
---
+++ src/helper/matrix.ts
@@ -0,0 +1,148 @@
+///<reference path="fraction.js.d.ts"/>
+import Fraction = require('../../node_modules/fraction.js/fraction');
+//import {Fraction} from 'fraction.js';
+
+
+
+interface MatrixOperation {
+    matrix;
+    operation;
+}
+
+/**
+ * Класс для работы с матрицами
+ */
+export class Matrix {
+    width: number;
+    height: number;
+    matrix: Fraction[][];
+
+    debugMatrix: MatrixOperation[] = [];
+
+    constructor(w, h, matrix?) {
+        this.width = w;
+        this.height = h;
+        if (matrix) {
+            this.matrix = matrix;
+        } else {
+            this.matrix = [];
+            for (let i = 0; i < h; i++) {
+                this.matrix[i] = [];
+                for (let j = 0; j < w; j++) {
+                    this.matrix[i].push(new Fraction(0));
+                }
+            }
+        }
+    }
+
+    copy() {
+        let matrix = [];
+        this.matrix.forEach((r) => {
+            matrix.push(r.slice());
+        });
+        return new Matrix(matrix[0].length, matrix.length, matrix);
+    }
+
+    set(pos: number[], value) {
+        let matrix = this.matrix;
+        const i = +pos[0], j = +pos[1];
+        if (pos.length == 2 && matrix[i] && matrix[i][j]) {
+            matrix[i][j] = value;
+        } else {
+            console.error(pos, matrix, 'out of array');
+        }
+    }
+
+    get() {
+        return this.matrix;
+    }
+
+    gauss(debug: boolean = false) {
+        const mh = this.matrix.length;
+        const mw = this.matrix[0].length;
+        let tmp;
+        if (debug) {
+            this.debugMatrix = [];
+        }
+
+        for (let i = 0; i < mh; i++) {
+            let el = this.matrix[i][i];
+            if (el.n == 0) {
+                for (let j = i + 1; j < mw; j++) {
+                    el = this.matrix[j][i];
+                    if (el.n != 0) {
+                        if (debug) {
+                            this.debugMatrix.push({
+                                matrix: null,
+                                operation: `(${i})${arrFractionToStr(this.matrix[i])} + (${j})${arrFractionToStr(this.matrix[j])}`
+                            });
+                        }
+
+                        for (let k = 0; k < mw; k++) {
+                            this.matrix[i][k].add(this.matrix[j][k]);
+                        }
+
+                        if (debug) {
+                            this.debugMatrix[this.debugMatrix.length - 1].matrix = this.copy();
+                        }
+                        break;
+                    }
+                }
+            }
+            if (el.n != 0) {
+                for (let j = i + 1; j < mh; j++) {
+                    let c = this.matrix[j][i].neg().div(el);
+                    if (c.n == 0) {
+                        break;
+                    }
+
+                    if (debug) {
+                        this.debugMatrix.push({
+                            matrix: null,
+                            operation: `(${j})${arrFractionToStr(this.matrix[j])} + ${c.toFraction()} * (${i})${arrFractionToStr(this.matrix[i])}`
+                        });
+                    }
+
+                    for (let k = mw - 1; k >= i; k--) {
+                        tmp = c.mul(this.matrix[i][k]);
+                        this.matrix[j][k] = this.matrix[j][k].add(tmp);
+                    }
+
+                    if (debug) {
+                        this.debugMatrix[this.debugMatrix.length - 1].matrix = this.copy();
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    toString() {
+        let buf = '';
+        this.matrix.forEach(row => {
+            let str = '[';
+            row.forEach(el => {
+                str += ' ' + el.toFraction();
+            });
+            buf += str + ']\n';
+        });
+        return buf;
+    }
+}
+
+function arrFractionToStr(arr: Fraction[]) {
+    let str = arr.reduce((pr, e) => pr + ", " +e.toFraction());
+    return `[${str}]`;
+}
+
+function copyMatrix(matr) {
+    let matrix = [];
+
+    matr.forEach((row, i) => {
+        matrix.push([]);
+        row.forEach(el => {
+            matrix[i].push(el);
+        });
+    });
+    return matrix;
+}
